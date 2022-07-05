2b5acb1a145415d4029ef84a5fa54ebff491b84d
Mahima Tewatia <mahima.tewatia@itsacheckmate.com>
2022-07-05 14:56:59 +0530

    Modified navbar file, omniauth_controller file

--- app/models/twitter_account.rb
+++ app/models/twitter_account.rb
@@ -5,8 +5,8 @@ class TwitterAccount < ApplicationRecord
 
   def client
     Twitter::REST::Client.now do |config|
-      config.consumer_key         = "Rails.application.credentials.dig(:twitter, :api_key)"
-      config.consumer_secret      = "Rails.application.credentials.dig(:twitter, :api_secret)"
+      config.consumer_key         = Rails.application.credentials.dig(:twitter, :api_key)
+      config.consumer_secret      = Rails.application.credentials.dig(:twitter, :api_secret)
       config.access_token         = token
       config.access_token_secret  = secret
     end
