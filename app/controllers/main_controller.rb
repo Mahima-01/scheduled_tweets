e5141ed11ad4a9263bf27810b9c7a0333bdcd3d1
Mahima Tewatia <mahima.tewatia@itsacheckmate.com>
2022-07-05 13:05:53 +0530

    Creating a github repo to store our code.

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,9 @@
+class MainController < ApplicationController
+  def index
+    #flash.now[:notice] = "Logged in successfully"
+    #flash.now[:alert] = "Invalid email or password"
+    
+  end
+end
+
+  
---
+++ app/models/tweet.rb
@@ -0,0 +1,27 @@
+class Tweet < ApplicationRecord
+  belongs_to :user
+  belongs_to :twitter_account
+
+  validates :body, length: { minimum:1, maximum: 200 }
+  validates :published_at, presence: true
+
+  after_initialize do
+    self.published_at ||= 24.hours.from_now
+  end
+
+  after_save_commit do
+    if published_at_previously_changed?
+      TweetJob.set(wait_until: published_at).perform_later(self)
+    end
+  end
+
+  def published?
+    tweet_id?
+  end
+
+  def publish_to_twitter!
+    tweet = twitter_account.client.update(body)
+    update(tweet_id: tweet.id)
+  end
+end
+
